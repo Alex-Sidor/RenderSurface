acbe0285172552403b349e3fe244635413a862e7
alex-sidor <astro_studios@outlook.com>
2025-10-04 17:47:00 +0100

    completion of last commit

--- src/Screen.cpp
+++ src/Screen.cpp
@@ -1,25 +1,111 @@
 #include "Screen.h"
+#include <stdexcept>
 #include <iostream>
+#include <vector>
 
-Screen::Screen(int x, int y) : width(x), height(y) {
+static const char* vertexShaderSource = R"glsl(
+#version 460 core
+layout(location = 0) in vec2 aPos;
+layout(location = 1) in vec2 aTexCoord;
+out vec2 TexCoord;
+void main()
+{
+    TexCoord = aTexCoord;
+    gl_Position = vec4(aPos, 0.0, 1.0);
+}
+)glsl";
+
+static const char* fragmentShaderSource = R"glsl(
+#version 460 core
+out vec4 FragColor;
+in vec2 TexCoord;
+uniform sampler2D screenTex;
+void main()
+{
+    FragColor = texture(screenTex, TexCoord);
+}
+)glsl";
+
+bool Screen::shouldClose() {
+    return glfwWindowShouldClose(window);
+}
+
+Screen::Screen(int x, int y, const char* name) : width(x), height(y) {
     if (!glfwInit()) {
-        error = "Failed to initialize GLFW";
-        throw std::runtime_error(error);
+        throw std::runtime_error("Failed to initialize GLFW");
     }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-
-    window = glfwCreateWindow(width, height, "FastEngine", nullptr, nullptr);
+    window = glfwCreateWindow(width, height, name, nullptr, nullptr);
     if (!window) {
         glfwTerminate();
         throw std::runtime_error("Failed to create GLFW window");
     }
+
     glfwMakeContextCurrent(window);
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         throw std::runtime_error("Failed to initialize GLAD");
     }
+
+    glViewport(0, 0, width, height);
+    glClearColor(0.f, 0.f, 0.f, 1.f);
+
+    glfwSwapInterval(1);
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+
+    GLint success;
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        char log[512];
+        glGetShaderInfoLog(vertexShader, 512, nullptr, log);
+        throw std::runtime_error(std::string("Vertex shader failed: ") + log);
+    }
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        char log[512];
+        glGetShaderInfoLog(fragmentShader, 512, nullptr, log);
+        throw std::runtime_error(std::string("Fragment shader failed: ") + log);
+    }
+
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    float vertices[] = {
+        -1.0f,  1.0f,  0.0f, 1.0f,
+        -1.0f, -1.0f,  0.0f, 0.0f,
+         1.0f, -1.0f,  1.0f, 0.0f,
+
+        -1.0f,  1.0f,  0.0f, 1.0f,
+         1.0f, -1.0f,  1.0f, 0.0f,
+         1.0f,  1.0f,  1.0f, 1.0f
+    };
+
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
@@ -27,40 +113,36 @@ Screen::Screen(int x, int y) : width(x), height(y) {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0,
-        GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
-
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(0, width, height, 0, -1, 1);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0,
+                 GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
 }
 
 Screen::~Screen() {
-    if (texture) glDeleteTextures(1, &texture);
+    glDeleteTextures(1, &texture);
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    glDeleteProgram(shaderProgram);
     if (window) {
         glfwDestroyWindow(window);
         glfwTerminate();
     }
 }
 
-void Screen::displayBuffer(const uint32_t* buffer) {
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height,
-        GL_RGBA, GL_UNSIGNED_INT_8_8_8_8, buffer);
-
+void Screen::updateScreen() {
     glClear(GL_COLOR_BUFFER_BIT);
-    glEnable(GL_TEXTURE_2D);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0.0f, 0.0f); glVertex2f(0.0f, 0.0f);
-    glTexCoord2f(1.0f, 0.0f); glVertex2f((float)width, 0.0f);
-    glTexCoord2f(1.0f, 1.0f); glVertex2f((float)width, (float)height);
-    glTexCoord2f(0.0f, 1.0f); glVertex2f(0.0f, (float)height);
-    glEnd();
-    glDisable(GL_TEXTURE_2D);
+
+    glUseProgram(shaderProgram);
+    glBindVertexArray(vao);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glUniform1i(glGetUniformLocation(shaderProgram, "screenTex"), 0);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
+}
+
+void Screen::setTexture(const uint32_t* buffer) {
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
 }
--- src/Screen.h
+++ src/Screen.h
@@ -1,26 +1,22 @@
 #pragma once
 
-#include <cstdint>
-#include <stdexcept>
-#include <string>
-
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <cstdint>
+
 class Screen {
 public:
-    Screen(int x, int y);
+    Screen(int x, int y, const char* name);
     ~Screen();
-
-    void displayBuffer(const uint32_t* buffer);
-
-    std::string error;
-
-    GLFWwindow* window = nullptr;
+    void updateScreen();
+    void setTexture(const uint32_t* buffer);
+    bool shouldClose();
 
 private:
-    int width;
-    int height;
-
-    GLuint texture = 0;
+    int width, height;
+    GLFWwindow* window = nullptr;
+    unsigned int texture = 0;
+    unsigned int vao = 0, vbo = 0;
+    unsigned int shaderProgram = 0;
 };
